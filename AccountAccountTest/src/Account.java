f1d77f18cbc159302fd1f06323b0e2721e03c758
Nicat-Axundzada <nicataxundzada@gmail.com>
2024-03-14 13:45:44 +0400

    Account and AccountTest

---
+++ AccountAccountTest/src/Account.java
@@ -0,0 +1,14 @@
+public class Account {
+    private String name; // instance variable
+    int accoutNumber; // default is public
+
+    // method to set the name in the object
+    public void setName(String name) {
+        this.name = name;
+    }
+    // method to retrieve the name from the object
+    public String getName() {
+        return name;
+    }
+
+}
